ce93f0692f5b6150022263b3eb6501d216496c7e
mwilliford <mwilliford@21341639-f296-4de2-9f5a-af270d55a0a9>
2010-03-03 04:01:15 +0000

    Added some handles to prevent a different browser from being called, we should never allow the android default browser to handle things.
    git-svn-id: file:///var/svn/repos@46 21341639-f296-4de2-9f5a-af270d55a0a9

--- vizio/VizioMarket/src/com/mvw/vizio/webkit/MyVizioJSHandler.java
+++ vizio/VizioMarket/src/com/mvw/vizio/webkit/MyVizioJSHandler.java
@@ -1,5 +1,18 @@
 package com.mvw.vizio.webkit;
 
+import android.content.Context;
+import android.telephony.TelephonyManager;
+
+import com.mvw.vizio.activity.MarketHome;
+
 public class MyVizioJSHandler {
+	public String getPhoneNum() {
+		TelephonyManager telephony = (TelephonyManager)MarketHome.getThisContext().getSystemService(Context.TELEPHONY_SERVICE); 
+		return telephony.getLine1Number();
+	}
+	public String getId() {
+		TelephonyManager telephony = (TelephonyManager)MarketHome.getThisContext().getSystemService(Context.TELEPHONY_SERVICE); 
+		return telephony.getDeviceId();
+	}
 
 }
