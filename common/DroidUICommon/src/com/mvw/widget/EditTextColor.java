1079f876eca4c325ba88d88e984ec3f9bfa81586
mwilliford <mwilliford@21341639-f296-4de2-9f5a-af270d55a0a9>
2010-03-07 01:32:03 +0000

    Initial import
    git-svn-id: file:///var/svn/repos@52 21341639-f296-4de2-9f5a-af270d55a0a9

---
+++ common/DroidUICommon/src/com/mvw/widget/EditTextColor.java
@@ -0,0 +1,21 @@
+package com.mvw.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+public class EditTextColor extends EditText {
+
+	public EditTextColor(Context context) {
+		super(context);
+	}
+
+	public EditTextColor(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	public EditTextColor(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+}
