307432c03f86ae0bc5b7698ce3e661696f2deb06
mwilliford <mwilliford@21341639-f296-4de2-9f5a-af270d55a0a9>
2010-03-10 06:32:20 +0000

    some comments.
    git-svn-id: file:///var/svn/repos@74 21341639-f296-4de2-9f5a-af270d55a0a9

--- trunk/common/android/DroidCommand/src/com/mvw/command/Command.java
+++ trunk/common/android/DroidCommand/src/com/mvw/command/Command.java
@@ -3,6 +3,9 @@ package com.mvw.command;
 import com.mvw.event.CommandEvent;
 
 public interface Command {
+	/*
+	 * This method will be called when a configured event is fired.
+	 */
 	public void execute(CommandEvent event);
 
 }
