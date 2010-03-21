f007a6b7672c01ba5d1e6496104f86faca3f601c
smineyev <smineyev@21341639-f296-4de2-9f5a-af270d55a0a9>
2010-03-21 03:56:50 +0000

    see #39
    git-svn-id: file:///var/svn/repos@93 21341639-f296-4de2-9f5a-af270d55a0a9

--- trunk/common/android/DroidCommand/src/com/mvw/controller/CommandMappingException.java
+++ trunk/common/android/DroidCommand/src/com/mvw/controller/CommandMappingException.java
@@ -2,13 +2,11 @@ package com.mvw.controller;
 
 public class CommandMappingException extends Exception {
 
+	private static final long serialVersionUID = 7548673498237833035L;
+
 	public CommandMappingException(String string) {
 		super(string);
 	}
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 
 }
